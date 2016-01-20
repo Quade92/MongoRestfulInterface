d99376aae7780e5dff3f1d6b85d0a32bbfa6bf63
juiceyang <abandonclock@gmail.com>
2016-01-20 16:11:44 +0800

    modify response style use request.authorization to accomplish basic http authentication

--- app/main/models.py
+++ app/main/models.py
@@ -7,23 +7,38 @@ from pymongo.errors import OperationFailure
 
 class LastRecord(Resource):
     def get(self):
-        data = request.data
-        l = loads(data)
         mongo = MongoClient(host="localhost", port=27999)
         try:
-            mongo.sensorLog.authenticate(l["username"], l["pwd"])
+            mongo.sensorLog.authenticate(request.authorization.username, request.authorization.password)
             # return the latest record
             last_log = mongo["sensorLog"]["sensorlogs"].find().sort("_id", -1)[0]
-            return {"err": "False", "result": {"data": dumps(last_log)}}
+            return {
+                       "err": "False",
+                       "message": "Successfully get data",
+                       "result": {"data": dumps(last_log)}
+            }
         except OperationFailure, err:
-            return {"err": "True","result": err.details}
+            return {
+                "err": "True",
+                "message": "Failed getting data",
+                "result": err.details
+            }
+
     def post(self):
-        data = request.data
-        l = loads(data)
         mongo = MongoClient(host="localhost", port=27999)
         try:
-            mongo.sensorLog.authenticate(l["username"], l["pwd"])
+            mongo.sensorLog.authenticate(request.authorization.username, request.authorization.password)
+            data = request.data
+            l = loads(data)
             result = mongo["sensorLog"]["sensorlogs"].insert_one(l["data"])
-            return {"err": "False", "result": {"data": dumps(l["data"]), "ack": str(result.acknowledged)}}
+            return {
+                "err": "False",
+                "message": "Successfully post data",
+                "result": {"data": dumps(l["data"]), "ack": str(result.acknowledged)}
+            }
         except OperationFailure, err:
-            return {"err": "True", "result": err.details}
+            return {
+                "err": "True",
+                "message": "Failed post data",
+                "result": err.details
+            }
