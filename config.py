71907dfc6c29e30dc50712f824d9487f7f1b6163
juiceyang <abandonclock@gmail.com>
2016-02-26 10:50:32 +0800

    refactor db name and collection name

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ db_config = {
     "dev_config": {
         "host": "localhost",
         "port": 27999,
-        "db": "sensorLog",
-        "collection": "test"
+        "db": "raw_data_db",
+        "collection": "raw_data"
     }
 }
