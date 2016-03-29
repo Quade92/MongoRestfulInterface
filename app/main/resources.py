cea5ceaccacfd97b7e74bb785c5b7919543dcfa2
juiceyang <abandonclock@gmail.com>
2016-03-29 10:24:32 +0800

    modify record post

--- app/main/resources.py
+++ app/main/resources.py
@@ -15,34 +15,41 @@ class BaseClassWithCORS(flask_restful.Resource):
         resp = flask.make_response("")
         resp.headers.extend({
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST",
+            "Access-Control-Allow-Methods": "GET,POST",
             "Access-Control-Allow-Headers": "Authorization,Content-Type"
         })
         return resp
 
 
 class RecordSeries(BaseClassWithCORS):
-    def get(self, start, end):
+    def post(self, start, end):
         data_db_host = config.db_config[config.config_name]["data_db"]["host"]
         data_db_port = config.db_config[config.config_name]["data_db"]["port"]
-        mongo = flask_pymongo.MongoClient(host=data_db_host,port=data_db_port)
+        data_db_mongo = flask_pymongo.MongoClient(host=data_db_host, port=data_db_port)
+        db = config.db_config[config.config_name]["data_db"]["db"]
+        data_db = data_db_mongo[db]
         try:
-            mongo[config.db_config[config.config_name]["data_db"]].authenticate(flask.request.authorization.username,
-                                                                             flask.request.authorization.password)
-            # QUERY: return the latest record
-            # TODO records length need some restriction maybe
-            records = mongo[config.db_config[config.config_name]["data_db"]]\
-                [config.db_config[config.config_name]["trans_data_col"]].find({
-                "$and": [{"timestamp": {"$gte": start}},
-                         {"timestamp": {"$lte": end}}]}
-            ).sort("_id", -1)
-            records = [record for record in records]
-            data = {
-                "err": "False",
-                "message": "Successfully auth",
-                "result": records
-            }
-            resp = flask.make_response(dumps(data))
+            request_data = loads(flask.request.data)
+            token = request_data["token"]
+            checked = factory.auth_db["token"].find({"token": token})
+            if checked:
+                records = data_db.find({
+                    "$and": [{"timestamp": {"$gte": start}},
+                             {"timestamp": {"$lte": end}}]}
+                ).sort("_id", -1)
+                records = [record for record in records]
+                resp_data = {
+                    "err": "False",
+                    "message": "Successfully auth",
+                    "result": records
+                }
+            else:
+                resp_data = {
+                    "err": "True",
+                    "message": "Please login",
+                    "result": ""
+                }
+            resp = flask.make_response(dumps(resp_data))
             resp.headers.extend({
                 "Access-Control-Allow-Origin": "*"
             })
@@ -61,7 +68,7 @@ class LatestRecord(BaseClassWithCORS):
                                           port=config.db_config[config.config_name]["port"])
         try:
             mongo[config.db_config[config.config_name]["data_db"]].authenticate(flask.request.authorization.username,
-                                                                             flask.request.authorization.password)
+                                                                                flask.request.authorization.password)
             # QUERY: return the latest record
             latest_record = mongo[config.db_config[config.config_name]["data_db"]] \
                 [config.db_config[config.config_name]["trans_data_col"]].find().sort("_id", -1)[0]
@@ -85,28 +92,36 @@ class LatestRecord(BaseClassWithCORS):
 
 class Record(BaseClassWithCORS):
     def post(self):
-        mongo = flask_pymongo.MongoClient(host=config.db_config[config.config_name]["host"],
-                                          port=config.db_config[config.config_name]["port"])
+        data_db_host = config.db_config[config.config_name]["data_db"]["host"]
+        data_db_port = config.db_config[config.config_name]["data_db"]["port"]
+        data_db_mongo = flask_pymongo.MongoClient(host=data_db_host, port=data_db_port)
+        db = config.db_config[config.config_name]["data_db"]["db"]
+        data_db = data_db_mongo[db]
+        raw_col = config.db_config[config.config_name]["raw_data_col"]
+        trans_col = config.db_config[config.config_name]["trans_data_col"]
         try:
-            mongo[config.db_config[config.config_name]["data_db"]].authenticate(flask.request.authorization.username,
-                                                                             flask.request.authorization.password)
-            data = flask.request.data
-            # TODO: insert transformed data here
-            raw_req = loads(data)
-            raw_json = raw_req["data"]
-            trans_json = config.transform_data(raw_json)
-            raw_insert_result = mongo[config.db_config[config.config_name]["data_db"]] \
-                [config.db_config[config.config_name]["raw_data_col"]].insert_one(raw_json)
-            trans_insert_result = mongo[config.db_config[config.config_name]["data_db"]] \
-                [config.db_config[config.config_name]["trans_data_col"]].insert_one(trans_json)
-            resp_data = {
-                "err": "False",
-                "message": "Successfully inserted",
-                "result": {
-                    "raw_insert_id": raw_insert_result.inserted_id,
-                    "trans_insert_id": trans_insert_result.inserted_id
+            request_data = loads(flask.request.data)
+            token = request_data["token"]
+            checked = factory.auth_db["token"].find({"token": token})
+            if checked:
+                raw_json = request_data["data"]
+                trans_json = config.transform_data(raw_json)
+                raw_insert_result = data_db[raw_col].insert_one(raw_json)
+                trans_insert_result = data_db[trans_col].insert_one(trans_json)
+                resp_data = {
+                    "err": "False",
+                    "message": "Successfully inserted",
+                    "result": {
+                        "raw_insert_id": raw_insert_result.inserted_id,
+                        "trans_insert_id": trans_insert_result.inserted_id
+                    }
                 }
-            }
+            else:
+                resp_data = {
+                    "err": "True",
+                    "message": "Please login",
+                    "result": ""
+                }
             resp = flask.make_response(dumps(resp_data))
             resp.headers.extend({
                 "Access-Control-Allow-Origin": "*"
@@ -130,7 +145,7 @@ class AuthenticateByPassword(BaseClassWithCORS):
                 token = uuid.uuid4().hex
                 now = datetime.datetime.utcnow()
                 expir = datetime.timedelta(days=7)
-                expir_timestamp = int(((now+expir) - datetime.datetime(1970,1,1)).total_seconds()*1000)
+                expir_timestamp = int(((now + expir) - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
                 factory.auth_db["token"].insert({"token": token, "expir": expir_timestamp})
                 data = {
                     "err": "False",
@@ -162,7 +177,7 @@ class LatestRecordSet(BaseClassWithCORS):
                                           port=config.db_config[config.config_name]["port"])
         try:
             mongo[config.db_config[config.config_name]["data_db"]].authenticate(flask.request.authorization.username,
-                                                                             flask.request.authorization.password)
+                                                                                flask.request.authorization.password)
             latest_records = mongo[config.db_config[config.config_name]["data_db"]] \
                                  [config.db_config[config.config_name]["trans_data_col"]] \
                                  .find().sort("_id", -1)[:amount]
