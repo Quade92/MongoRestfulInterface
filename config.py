7b684408f5d35707c789c73226ed08c1610461e9
juiceyang <abandonclock@gmail.com>
2016-05-16 10:49:09 +0800

    update transform relation

--- config.py
+++ config.py
@@ -21,17 +21,33 @@ db_config = {
 }
 
 
-def transform_data(raw_json):
+def transform_data(window, raw_json):
     trans_json = {}
     trans_json["dtu_id"] = raw_json["dtu_id"]
     trans_json["timestamp"] = raw_json["timestamp"]
     trans_json["channel"] = {}
     new_labels = [u"A路电机转速", u"B路电机转速", u"海流流速", u"海流流向", u"A路电机输出电压", u"B路电机输出电压",
-                  u"A路电机输出电流", u"B路电机输出电流", u"A路电机功率", u"B路电机功率"]
+                  u"A路电机输出电流", u"B路电机输出电流", u"A路电机功率", u"B路电机功率", u"累计发电量"]
     for index, label in enumerate(new_labels):
         trans_json["channel"]["CH" + str(index+1)] = {
             "label": label,
             "value": 10*index+random.random()-0.5
         }
+    trans_json["channel"]["CH5"] = {
+        "label": new_labels[4],
+        "value": round(15.06 * raw_json["sensors"]["AN1"]["value"]+16.38,2)
+    }
+    trans_json["channel"]["CH6"] = {
+        "label": new_labels[5],
+        "value": round(15.06 * raw_json["sensors"]["AN2"]["value"]+16.38,2)
+    }
+    trans_json["channel"]["CH7"] = {
+        "label": new_labels[6],
+        "value": round(9.392 * raw_json["sensors"]["AN7"]["value"]-0.9334,2)
+    }
+    trans_json["channel"]["CH8"] = {
+        "label": new_labels[7],
+        "value": round(9.054 * raw_json["sensors"]["AN8"]["value"] - 0.8262,2)
+    }
     # TODO modify value in tran_json.channel
     return trans_json
--- run.py
+++ run.py
@@ -5,4 +5,4 @@ flask_app = create_app()
 
 print "login succeed!"
 if __name__ == '__main__':
-    flask_app.run(threaded=True)
+    flask_app.run(host="0.0.0.0", threaded=True)
